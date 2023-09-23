a0fe54e40614bea6eecc0d2ce56a7bf00005168b
krishnapandri <krishnapandri001@gmail.com>
2023-09-23 23:47:19 +0530

    latest commit 23 sept 23

--- src/Components/taskForm.jsx
+++ src/Components/taskForm.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../CSS/taskForm.css"
+import { render } from "react-dom";
 
 function TaskForm(){
     const [totalCount,setTotalCount] = useState(0);
     const [completed,setCompletedTask] = useState(0);
-    const [taskList,setTaskList] = useState([{task:"NewTask",Completed:false}]);
-    const [val , setval]  = useState('') 
+    const [taskList,setTaskList] = useState([{task:"NewTask",Completed:false,isFavourite:false}]);
+    const [val , setval]  = useState('') ;
+    const [isFavourite,setisFavourite] = useState(false);
     function Addtask(){
-        setTaskList([...taskList,{task:val,completed:false}])
+        setTaskList([...taskList,{task:val,completed:false,isFavourite:false}])
         setval('')
     }
     function settask(e){
          setval(e.currentTarget.value) 
+    }
+    useEffect(function(){
+         setTotalCount(taskList.length)
+    },[taskList])
+
+    function setfav(mind){
+        
     }
     return(
         <div>
@@ -26,18 +35,25 @@ function TaskForm(){
        {
         taskList.map(function(ele,ind){
             return(
-                <div>
-                <div className="listbox" key={ind}>
+                <div key={ind}>
+                <div className="listbox" >
                                 <div className="list d-flex justify-content-between">
                                     <div><b>{ind + 1}.</b><u>{ele.task}</u></div>
-                                    <div className="d-flex justify-content-around align-items-center" style={{width:"22%"}}>
-                                        <i className="fa-solid fa-heart" id="heart"></i>
-                                        <i className="fa-solid fa-trash"></i> 
-                                        <i className="fa-solid fa-check-double" style={{color: '#26e5f2'}}></i>
-                                        <i className="fa-regular fa-pen-to-square" style={{color: '#d99f20'}}></i>
+                                <div className="d-flex justify-content-around align-items-center" style={{width:"22%"}}>
+                            <div onClick={setfav.bind(null,ind)}>
+                                {
+                                    <div>
+                                        <i className={"fa-regular  fa-heart"} id="heart"></i>
+                                        {/* <i className={" fa-solid fa-heart"} id="heart"></i> */} 
                                     </div>
-                                </div>
+                                }
                             </div>
+                                <i className="fa-solid fa-trash"></i> 
+                                <i className="fa-solid fa-check-double" style={{color: '#26e5f2'}}></i>
+                                <i className="fa-regular fa-pen-to-square" style={{color: '#d99f20'}}></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )
         })
